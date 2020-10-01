16ae971746dec787d0cf5d647f6d46819a193d74
Kuba Rożek <jakub.rozek@gmail.com>
2020-10-01 12:58:17 +0200

    feat: docker lib copy-paste

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "log"
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
 
 func main() {
-	log.Println("Hello world!!!")
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
+	}
 }
